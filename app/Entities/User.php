0d921ccb43ffff70751db2a86a1a07f8e769f9f5
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-02-01 10:46:10 +0330

    entities directory

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Entities;
 
+use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
