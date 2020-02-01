6e95a722d050270ba7bd56ce4f05c5868ded7d0d
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-02-01 11:04:12 +0330

    repository

---
+++ app/Repositories/ExampleRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\Repositories\ExampleRepositoryInterface;
+
+class ExampleRepository implements ExampleRepositoryInterface
+{
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+}
