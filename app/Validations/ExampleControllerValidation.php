729e92af8e02956f52f64047bbc601e57a923755
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-02-02 18:06:23 +0330

    validations

---
+++ app/Validations/ExampleControllerValidation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Validations;
+
+class ExampleControllerValidation
+{
+    public function getAll()
+    {
+        return [
+            // validation rule
+        ];
+    }
+}
